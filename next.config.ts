f1f8deccae96134c8bcda53eeceb5b43cebdd392
dhruvG110 <guptadhruv472@gmail.com>
2025-09-20 13:15:21 +0530

    Again improvised 14

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- 
+  experimental: {
+    appDir: true,
+    srcDir: 'src',   // <-- tell Next.js the app folder is inside src
+  },
 };
 
 export default nextConfig;
